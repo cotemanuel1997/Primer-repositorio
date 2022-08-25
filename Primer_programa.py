332b433def4994cc34cea68c934dfa701568a6fe
Emanuel Cot <cotemanuel1997>
2022-08-25 19:26:40 -0300

    Agrego al programa la capacidad de concatenar dos cadenas de texto, que son 'Hola' y 'mundo' y ademas que muestre esta concatenación por pantalla

--- Primer_programa.py
+++ Primer_programa.py
@@ -14,5 +14,13 @@ def main():
      numero2=4
      print(numero1+numero2)
      # Suma de los numeros enteros 3 y 4 y además se muestra el resultado por pantalla
+     string1="Hola "
+     string2="mundo"
+     print(string1+string2)
+     '''
+     Se muestra por pantalla la concatenación de las variables
+     string1 y string2, cuyos valores son las cadenas de texto "Hola" y "mundo"
+     respectivamente
+     '''
 if __name__ == '__main__':
     main()
