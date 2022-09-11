4b815c32c99323049e0468a307a7cf56dafb5e4c
Emanuel Cot <cotemanuel1997>
2022-09-11 18:45:59 -0300

    Agrego el ejercicio 1 de funciones

---
+++ EjercicioFunciones_1.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Emanuel
+#
+# Created:     10/09/2022
+# Copyright:   (c) Emanuel 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    def area_rectangulo(base,altura):
+        return base*altura
+
+    print(area_rectangulo(3,4))
+
+
+if __name__ == '__main__':
+    main()
