28a7c965eb507b0805931ae451c2a4e88eb55476
Emanuel Cot <cotemanuel1997>
2022-09-11 18:59:24 -0300

    Agrego el ejercicio 4 de funciones

---
+++ EjercicioFunciones_4.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Emanuel
+#
+# Created:     10/09/2022
+# Copyright:   (c) Emanuel 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    def intermedio(num1,num2):
+        return (num1 + num2)/2
+    print(intermedio(10,24))
+    print(intermedio(12,50))
+
+if __name__ == '__main__':
+    main()
