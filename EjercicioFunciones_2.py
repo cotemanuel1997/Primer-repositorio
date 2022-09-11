7bd6f85bfef6003b8f037b9d011e77deab3398c0
Emanuel Cot <cotemanuel1997>
2022-09-11 18:57:24 -0300

    Agrego el ejercicio 2 de funciones

---
+++ EjercicioFunciones_2.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Emanuel
+#
+# Created:     10/09/2022
+# Copyright:   (c) Emanuel 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    def area_circulo(radio):
+        return radio ** 2 * 3.14159
+    print(area_circulo(4))
+
+if __name__ == '__main__':
+    main()
